803de5bf6f8951278db6714ec9b4fd8730b03820
xiaoxiaoliner <jxl007@yeah.net>
2020-10-30 18:15:24 +0800

    kk

--- hwll.go
+++ hwll.go
@@ -124,7 +124,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // tmpl is the HTML template that drives the user interface.
 var tmpl = template.Must(template.New("tmpl").Parse(`
-<!DOCTYPE html><html><body><center>
+<!DOCTYPE html><html><body><center> 
 	<h2>Is Go {{.Version}} out yet?</h2>
 	<h1>
 	{{if .Yes}}
